51ed96bbbd19dc4234b214b733cea74d16c197ca
Varsha Addagulla <varsha.addagulla@gmail.com>
2026-04-22 13:02:30 -0500

    Use Flask built-in server for Render deployment, remove gunicorn

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ def assess():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
